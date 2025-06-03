6f39ce43fb5340001d1d602fd1fa630d1eeae3bc
R0drigOO <marquesrodrigo@ua.pt>
2025-06-03 17:22:53 +0100

    removed unused import

--- WashNowBackEnd/src/test/java/tqs/WashNow/functional/ReservationUITest.java
+++ WashNowBackEnd/src/test/java/tqs/WashNow/functional/ReservationUITest.java
@@ -10,7 +10,6 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 import org.springframework.boot.test.context.SpringBootTest;
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
