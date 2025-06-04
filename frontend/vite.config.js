1955085fc5cdc308b5632fda3862da682244481e
R0drigOO <marquesrodrigo@ua.pt>
2025-06-04 18:39:31 +0100

    mais uma try

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8080',
+        target: 'http://192.168.160.11:8080',
         changeOrigin: true,
         secure: false
       }
