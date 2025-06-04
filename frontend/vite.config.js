8524cc6233fa8516fe642eeaf083f8665792e86c
R0drigOO <marquesrodrigo@ua.pt>
2025-06-04 17:52:18 +0100

    frontend na vm sem acesso corrigido

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://deti-tqs-11.ua.pt:8080',
+        target: 'http://localhost:8080',
         changeOrigin: true,
         secure: false
       }
