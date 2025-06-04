f889e63a2d5ed9610959b79ab480c9a42e0bd4f0
R0drigOO <marquesrodrigo@ua.pt>
2025-06-04 16:06:50 +0100

    nomes dos testes dos controllers corrigidos

--- WashNowBackEnd/src/test/java/tqs/WashNow/controllers/WashSessionControllerTest.java
+++ WashNowBackEnd/src/test/java/tqs/WashNow/controllers/WashSessionControllerTest.java
@@ -16,7 +16,7 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.*;
 
-class WashSessionControllerIT {
+class WashSessionControllerTest {
 
     @Mock
     private WashSessionService washSessionService;
